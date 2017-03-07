a02a990a8f89fa1a5119e429b0b7bcade017eedc
Wesley Batista <wesleybatistasantos@gmail.com>
2017-03-07 09:01:15 -0300

    File upload to Google Cloud Storage

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 'use strict';
-
+require('dotenv').config();
 const Hapi = require('hapi');
 const fs = require('fs');
-
-
+const storage = require('@google-cloud/storage');
 const server = new Hapi.Server();
-server.connection({ port: 3000, host: 'localhost' });
+
+server.connection({ port: process.env.PORT || 5002 });
+
+const PROJECT_ID = process.env.PROJECT_ID;
+const PROJECT_JSON_KEY = process.env.PROJECT_JSON_KEY;
+const PROJECT_BUCKET = process.env.PROJECT_BUCKET;
+
+const CLIENT_CREDENTIALS = {
+  projectId: PROJECT_ID,
+  keyFilename: PROJECT_JSON_KEY
+};
+const gcs = storage(CLIENT_CREDENTIALS);
+const bucket = gcs.bucket(PROJECT_BUCKET);
+
+
 
 server.route({
     method: 'GET',
@@ -35,27 +48,43 @@ server.route({
         },
 
         handler: function (request, reply) {
+
+			function getPublicUrl (filename) {
+			  return `https://storage.googleapis.com/${PROJECT_BUCKET}/${filename}`;
+			}
+
             var data = request.payload;
-            console.log('POST /submit hit');
-            console.log(data)
             if (data.file) {
-                var name = data.file.hapi.filename;
-                var path = __dirname + "/uploads/" + name;
-                var file = fs.createWriteStream(path);
 
-                file.on('error', function (err) { 
-                    console.error(err) 
-                });
+            	var name = data.file.hapi.filename;
 
-                data.file.pipe(file);
+				var fileName = 'images/'+name;
 
-                data.file.on('end', function (err) { 
+				var file = bucket.file(fileName);
+				
+				const stream = file.createWriteStream({
+					metadata: {
+						contentType: data.file.hapi.headers["content-type"]
+					}
+				});
+
+				  stream.on('error', (err) => {
+				    data.cloudStorageError = err;
+				    reply(JSON.stringify(err));
+				  });
+				
+				  stream.on('finish', () => {
+				    data.cloudStorageObject = fileName;
+				    data.cloudStoragePublicUrl = getPublicUrl(fileName);
                     var ret = {
                         filename: data.file.hapi.filename,
-                        headers: data.file.hapi.headers
+                        headers: data.file.hapi.headers,
+                        downloadUrl: getPublicUrl(fileName)
                     }
                     reply(JSON.stringify(ret));
-                })
+				  });
+
+				  stream.end(data.file._data);
             }
 
         }
@@ -67,5 +96,7 @@ server.start((err) => {
     if (err) {
         throw err;
     }
+
     console.log(`Server running at: ${server.info.uri}`);
+
 });
